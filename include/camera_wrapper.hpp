bc79ff206243705c0a386a6a04c9d0b988229263
Adam Seewald <adam@seewald.email>
2022-05-31 17:42:05 -0400

    added the camera wrapper node for the sampling camera

---
+++ include/camera_wrapper.hpp
@@ -0,0 +1,31 @@
+
+#include "rclcpp/rclcpp.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <mutex>
+
+#ifndef YTCG_CAM_WRAPPER_HPP
+#define YTCG_CAM_WRAPPER_HPP
+
+#define NODE_CAM_WRAPPER "camera_wrapper_node"
+#define CAM_WRAPPER_RATE 500
+
+
+namespace ytcg {
+    class CameraWrapper : public rclcpp::Node {
+    public:
+        CameraWrapper(void);
+	~CameraWrapper(void);
+
+    private:
+	void timer_callback(void);
+        rclcpp::TimerBase::SharedPtr timer_;
+    };
+}
+
+#endif
+
+
---
+++ src/camera_wrapper.cpp
@@ -0,0 +1,45 @@
+
+#include "../include/camera_wrapper.hpp"
+#include "../include/utility.hpp"
+
+#include <functional>
+#include <cstdlib>
+#include <chrono>
+#include <cstdio>
+#include <limits>
+#include <cmath>
+
+using namespace ytcg;
+
+using std::placeholders::_1;
+
+
+CameraWrapper::CameraWrapper() : 
+    Node(NODE_CAM_WRAPPER) {
+
+    timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(CAM_WRAPPER_RATE), std::bind(&CameraWrapper::timer_callback, this)
+    );    
+}
+
+CameraWrapper::~CameraWrapper(void) { }
+
+void CameraWrapper::timer_callback(void) {
+    
+    std::string output, cam_command = "gphoto2 --capture-image";
+    output = utility_exec(cam_command.c_str());
+	
+    RCLCPP_INFO(this->get_logger(), "Took a picture with the sampling camera %s", output.c_str());
+
+}
+
+int main(int argc, char ** argv) {
+
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<CameraWrapper>());
+    rclcpp::shutdown();
+
+    return 0;
+}
+
+
