26bf686fbb31db35ddd7b8a539da48f01f174756
Adam Seewald <adam@seewald.email>
2022-10-13 23:21:06 -0400

    implemented obstacles node that takes info from pcd_wrapper and pose from SLAM to derive the next command for chw_ctl_comm node

--- include/obstacles.hpp
+++ include/obstacles.hpp
@@ -1,14 +1,14 @@
 
-#include "std_msgs/msg/multi_array_dimension.hpp"
-#include "std_msgs/msg/float32_multi_array.hpp"
-#include "std_msgs/msg/multi_array_layout.hpp"
+#include "message_filters/sync_policies/approximate_time.h"
+#include "message_filters/time_synchronizer.h"
 #include "std_msgs/msg/int8_multi_array.hpp"
-#include "sensor_msgs/msg/point_cloud2.hpp"
-#include "std_msgs/msg/float32.hpp"
+#include "message_filters/subscriber.h"
+#include "geometry_msgs/msg/point.hpp"
+#include "geometry_msgs/msg/pose.hpp"
 #include "rclcpp/rclcpp.hpp"
 
-#include <iostream>
 #include <fstream>
+#include <memory>
 #include <string>
 #include <vector>
 #include <mutex>
@@ -16,18 +16,74 @@
 #ifndef YTCG_OBSTACLES_HPP
 #define YTCG_OBSTACLES_HPP
 
-#define NODE_OBSTACLES      "obstacle_node"
+#define NODE_OBSTACLES                "obstacles"
+
+#define ORBSLAM_FRAMES_TOPIC          "/orb_slam3_ros_wrapper/pose"
+#define LONGEST_DISTANCE_POINT1_TOPIC "/pointcloud_depth_wrapper/ld_point1"
+#define LONGEST_DISTANCE_POINT2_TOPIC "/pointcloud_depth_wrapper/ld_point2"
 
 namespace ytcg {
+
+    struct Point3D {
+        double x, y, z;
+        Point3D(double _x, double _y, double _z) : x(_x), y(_y), z(_z) { }
+        Point3D() : x(.0), y(.0), z(.0) { }
+        Point3D& operator=(const geometry_msgs::msg::Pose::SharedPtr msg) {
+            x = msg->position.x;
+            y = msg->position.y;
+            z = msg->position.z;
+	    return *this;
+	}
+	Point3D& operator=(const geometry_msgs::msg::Point::ConstPtr& msg) {
+            x = msg->x;
+            y = msg->y;
+            z = msg->z;
+	    return *this;
+        }
+	Point3D& operator-(const Point3D& _point) {
+            x -= _point.x;
+            y -= _point.y;
+            z -= _point.z;
+            return *this;
+        }
+        Point3D& operator+(const Point3D& _point) {
+            x += _point.x;
+            y += _point.y;
+            z += _point.z;
+            return *this;
+        }
+        Point3D& operator/(const int val) {
+            x /= val;
+            y /= val;
+            z /= val;
+            return *this;
+        }
+    };
+
     class Obstacles : public rclcpp::Node {
     public:
         Obstacles(void);
 	~Obstacles(void);
 
     private:
-	void topic_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
-	rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
-        int count_ = 0;
+
+        void pose_topic_callback(const geometry_msgs::msg::Pose::SharedPtr);
+        void points_topic_callback(const geometry_msgs::msg::Point::ConstPtr&, const geometry_msgs::msg::Point::ConstPtr&);
+
+
+	typedef message_filters::sync_policies::ApproximateTime<
+                                      geometry_msgs::msg::Point,
+                                      geometry_msgs::msg::Point>    policy;
+	std::mutex                                                  _mutex;
+
+        std::shared_ptr<message_filters::Synchronizer<policy>>      sync_;
+        rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr   subscription___;
+        message_filters::Subscriber<geometry_msgs::msg::Point>      subscription__;
+        message_filters::Subscriber<geometry_msgs::msg::Point>      subscription_;
+        Point3D                                                     _point;
+
+        rclcpp::Publisher<std_msgs::msg::Int8MultiArray>::SharedPtr publisher_;
+	std_msgs::msg::Int8MultiArray                               msg_;
     };
 }
 
--- src/obstacles.cpp
+++ src/obstacles.cpp
@@ -1,4 +1,5 @@
 
+#include "../include/chw_bs_comm.hpp"
 #include "../include/obstacles.hpp"
 #include "../include/utility.hpp"
 
@@ -13,18 +14,81 @@ using namespace ytcg;
 using namespace std::chrono;
 
 using std::placeholders::_1;
+using std::placeholders::_2;
 
 
 Obstacles::Obstacles() : 
     Node(NODE_OBSTACLES) {
     
-    subscription_ == this->create_subscription<sensor_msgs::msg::PointCloud2>("/rtabmap/cloud_obstacles", 1, std::bind(&Obstacles::topic_callback, this, _1));
+    subscription_.subscribe(this, LONGEST_DISTANCE_POINT1_TOPIC);
+    subscription__.subscribe(this, LONGEST_DISTANCE_POINT2_TOPIC);
+
+    subscription___ = this->create_subscription<geometry_msgs::msg::Pose>(
+        ORBSLAM_FRAMES_TOPIC, 1, std::bind(&Obstacles::pose_topic_callback, this, _1)
+                                                                         );
+
+    RCLCPP_INFO_STREAM(this->get_logger(), "subscribed to " << ORBSLAM_FRAMES_TOPIC);
+
+    sync_ = std::make_shared<message_filters::Synchronizer<policy>>(policy(10), subscription_, subscription__);
+    sync_->registerCallback(std::bind(&Obstacles::points_topic_callback, this, _1, _2));
+
+    RCLCPP_INFO_STREAM(this->get_logger(), "subscribed to both " << 
+        LONGEST_DISTANCE_POINT1_TOPIC  << ", " << 
+        LONGEST_DISTANCE_POINT2_TOPIC << " using approximate time");
+
+    publisher_ = this->create_publisher<std_msgs::msg::Int8MultiArray>(COMM_FROM_BS_TOPIC, 10);
+
+    RCLCPP_INFO_STREAM(this->get_logger(), "created publisher for topic " << COMM_FROM_BS_TOPIC);
+    RCLCPP_INFO_STREAM(this->get_logger(), "node " << NODE_OBSTACLES << " is now initialized");
 }
 
 
-void Obstacles::topic_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
-    count_++;
-    RCLCPP_INFO(this->get_logger(), "%d time called", count_);
+void Obstacles::points_topic_callback(const geometry_msgs::msg::Point::ConstPtr& msg, const geometry_msgs::msg::Point::ConstPtr& _msg) {
+    
+    static size_t count_ = 0;
+    Point3D       ld_point1, ld_point2,
+                  waypoint,  waypoint_;
+    int           x_, y_;
+
+    msg_.data.clear();
+
+    if (count_++ == 0)
+        RCLCPP_INFO(this->get_logger(), "first points callback");   
+
+    ld_point1 = msg;
+    ld_point2 = _msg;
+    waypoint = (ld_point1+ld_point2)/2;
+    _mutex.lock();
+    waypoint_ = waypoint-_point;
+    _mutex.unlock();
+    x_ = 100*(waypoint_.x);
+    y_ = 100*(waypoint_.y);
+
+
+    msg_.data.push_back(x_);
+    msg_.data.push_back(y_);
+    
+    RCLCPP_INFO(this->get_logger(), "Publishing command x: %d, y: %d", x_, y_);
+    publisher_->publish(msg_);
+
+    RCLCPP_INFO(this->get_logger(), "next waypoint is (%f, %f, %f)", waypoint.x, waypoint.y, waypoint.z); 
+
+} 
+	
+
+void Obstacles::pose_topic_callback(const geometry_msgs::msg::Pose::SharedPtr msg) {
+
+    static size_t count_ = 0;
+
+    if (count_ == 0)
+        RCLCPP_INFO(this->get_logger(), "first pose callback");
+
+    _mutex.lock();
+    _point = msg;
+    _mutex.unlock();
+    
+    if (!(count_++ % 10)) // report pose every ten callbacks
+    	RCLCPP_INFO(this->get_logger(), "pose callback called %d times, pose (%f, %f, %f)", count_, msg->position.x, msg->position.y, msg->position.z);
 }
 
 Obstacles::~Obstacles(void) { }
